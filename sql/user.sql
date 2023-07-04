6fccbe9a1c741ca5ee8b1d9e3ea53b5c096b21ea
AdamWfeng <3246057843@qq.com>
2023-07-04 08:08:36 +0800

    fix: fix sql table
    fix createTime and updateTime

--- sql/user.sql
+++ sql/user.sql
@@ -11,8 +11,8 @@ create table user
     email        varchar(512)                       null comment '邮箱',
     userRole     int      default 0                 null,
     userStatus   int      default 0                 null comment '用户状态',
-    createTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '创建时间',
-    updateTime   datetime default CURRENT_TIMESTAMP null comment '更新时间',
+    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint  default 0                 null comment '逻辑删除'
 );
 
